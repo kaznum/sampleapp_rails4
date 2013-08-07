765a056b53431d373a9bddd7c993fb0b9ca9c4df
Kazuya NUMATA <numata@gmail.com>
2013-08-07 12:33:52 +0900

    Change root to hellos#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Sampleapp::Application.routes.draw do
+  get "hellos/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'hellos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
